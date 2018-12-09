a4258d9e19bfd2525080ed229952741373131c1a
Jacob Feldman <jacobgfeld@gmail.com>
2018-12-09 12:45:55 -0500

    creates a Post Component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Image from '../Image/Image.js'
+import TextPost from '../TextPost/TextPost.js'
+import './Post.css';
 
 const Post = (props) => {
   const isImage = props.post.url.match(/(.jpg|.gif|.jpeg|.png)$/);
-
   return (
     <div>
       {isImage ? (
-        <Image url={props.post.url}/>
+        <Image url={props.post.url} className='Post-Image'/>
         ) : (
-          props.post.text
+          props.post.text  === "" ? (
+            <a href={props.post.url}>{props.post.url}</a>
+          ) : (
+            <TextPost text={props.post.text}/>
+          )
         )
       }
-      <div onClick={props.returnHome}>Back</div>
     </div>
   )
 }
