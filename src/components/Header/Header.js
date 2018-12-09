19408f6a75566c53448f3c9fe3b04a60bf359a82
Jacob Feldman <jacobgfeld@gmail.com>
2018-12-09 13:44:25 -0500

    Changes style and name in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = (props) => {
   return (
     <div className='Header'>
-      <div onClick={props.returnHome} style={{padding: '14px'}}>Home</div>
+      <div onClick={props.returnHome} style={{ padding: '14px', cursor: 'pointer', float: 'left'}}>/r/programmerhumor</div>
     </div>
   )
 }
